47789cc073d989faaec980c5b26363b38b04cb52
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2020-03-10 10:51:30 -0700

    Added Delete Function to admin controller

--- VgWebApp/Controllers/AdminController.cs
+++ VgWebApp/Controllers/AdminController.cs
@@ -34,6 +34,16 @@ namespace VgWebApp.Controllers
 
         public ViewResult Index() => View(repository.Products);
         public ViewResult Create() => View("Edit", new Product());
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+            return RedirectToAction("Index");
+        }
 
     }
 }
