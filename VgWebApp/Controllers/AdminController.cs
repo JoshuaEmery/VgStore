0dd9d3870ed9231f2bdbe66177095ddf93e7677b
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2020-03-10 09:41:34 -0700

    Added Admin/Edit Action and View added Data Validation to Product.CS

--- VgWebApp/Controllers/AdminController.cs
+++ VgWebApp/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VgWebApp.Data;
 using System.Linq;
+using VgWebApp.Models;
 
 namespace VgWebApp.Controllers
 {
@@ -15,6 +16,21 @@ namespace VgWebApp.Controllers
         public ViewResult Edit(int productId) =>
             View(repository.Products
                 .FirstOrDefault(p => p.ProductID == productId));
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
+        }
 
         public ViewResult Index() => View(repository.Products);
     }
--- VgWebApp/Models/Product.cs
+++ VgWebApp/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,22 @@ namespace VgWebApp.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter a price")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please enter a genre")]
         public string Genre { get; set; }
+        [Required(ErrorMessage = "Please select if this game has multiplayer")]
         public bool Multiplayer { get; set; }
+        //I cannot remember whether we were going to assign games a rating
+        //or if the rating would be calculated??
+        //as it stands I did not include the Rating on the Product/Edit View
+        //[Required(ErrorMessage = "Please enter a product name")]
         public int Rating { get; set; }
+        [Required(ErrorMessage = "Please enter an ERSB rating")]
         public string ESRB { get; set; }
 
 
